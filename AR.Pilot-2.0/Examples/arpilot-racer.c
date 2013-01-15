8f351a5aad74e162f54f0f0bd166905166b4a602
Dirk Herrendoerfer <d.herrendoerfer@herrendoerfer.name>
2013-01-15 11:28:38 +0100

    Clean up joystick example * Fix update rate on example * add missing dependency to Makefile

--- AR.Pilot-2.0/Examples/arpilot-racer.c
+++ AR.Pilot-2.0/Examples/arpilot-racer.c
@@ -17,16 +17,6 @@
  *   along with arpilot.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* This is a short example of how to use the library for other
- * puropses.
- * Joystick racer example.
- *
- * With this example the drone can be controlled by a Microsoft XBOX 360
- * controller with the PC receiver. Layout is like in a racing game.
- * START starts the drone and BACK lands it.
- *
- * Enjoy :)*/
-
 
 #include <stdio.h>
 #include <fcntl.h>
@@ -34,6 +24,7 @@
 #include <sys/ioctl.h>
 #include <linux/joystick.h>
 
+#if 1
 #include "drone.h"
 
 #include "command.h"
@@ -42,6 +33,8 @@
 #include "log.h"
 #include "network.h"
 #include "web.h"
+
+#endif
 
 #define JOY_DEV "/dev/input/js0"
 
@@ -77,7 +70,7 @@ pedal_to_val(int pedal)
 
 int main()
 {
-        int joy_fd, *axis=NULL, num_of_axis=0, num_of_buttons=0, x;
+        int joy_fd, *axis=NULL, num_of_axis=0, num_of_buttons=0, x, count = 0;
         char *button=NULL, name_of_joystick[80];
         struct js_event js;
 
@@ -102,7 +95,7 @@ int main()
                 , num_of_buttons );
 
         fcntl( joy_fd, F_SETFL, O_NONBLOCK );   /* use non-blocking mode */
-
+#if 1
         /* Init libarpilot */
         if (setup_net())
         	exit(1);
@@ -122,8 +115,12 @@ int main()
 
         // Start the main loop
 
+#endif
+
         while( 1 )      /* infinite loop */
         {
+        		count++;
+
                 /* read the joystick state */
                 read(joy_fd, &js, sizeof(struct js_event));
 
@@ -138,34 +135,44 @@ int main()
                                 break;
                 }
 
+                /* print the results */
                 printf( "X: %6d  Y: %6d  ", axis_to_val(axis[0]), axis_to_val(axis[1]) );
                 printf("Z1: %6d  ", pedal_to_val(axis[2]) );
                 printf("Z2: %6d  ", pedal_to_val(axis[5]) );
 
                 roll = axis_to_val(axis[3]);
-                pitch = pedal_to_val(axis[2]) - pedal_to_val(axis[5]);
+
+                if (pedal_to_val(axis[2]) == 0 && pedal_to_val(axis[5]) == 0 )
+                	pitch = axis_to_val(axis[4]);
+                else
+                	pitch = pedal_to_val(axis[2]) - pedal_to_val(axis[5]);
+
                 gaz = axis_to_val(axis[1]);
                 yaw = axis_to_val(axis[0]);
 
                 printf("move: %6d,%6d,%6d,%6d,", roll,pitch,gaz,yaw);
-                command_move(roll,pitch,gaz,yaw); /*Move the drone*/
 
                 if (button[7] == 1 ) {
-                	command_state(1); /*Start the drone*/
+                	command_state(1);
                 	printf(" START ");
                 }
 
                 if (button[6] == 1 ){
-                	command_state(0);/*Land the drone*/
+                	command_state(0);
                 	printf(" LAND ");
                 }
-
                 printf("  \r");
                 fflush(stdout);
 
-                process(); /*Let the drone controller do its thing*/
+                if (count > 100) {
+                	command_move(roll,pitch,gaz,yaw);
+                	// Process drone data
+                	while (process() == 2)
+                		usleep(20000);
+                	count = 0;
+                }
         }
 
-        close( joy_fd );
+        close( joy_fd );        /* too bad we never get here */
         return 0;
 }
